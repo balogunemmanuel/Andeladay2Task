9c23c48152bd421be021fec96be92709d070b911
Balogun Emmanuel <dewddo@gmail.com>
2016-05-17 16:34:12 +0100

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,11 +1,36 @@
+// Javascript assignment
+
+var note_id;
+var note_content;
+var author;
 function NoteApplication(author) {
 	this.Author = author;
 	
-	var notes = []
+	this.notes[0] = note_id;
+	this.notes[1] = note_content;
+	this.notes[2] = author;
+}
+function create(note_content){
+	notes[1].push(note_content);
+}
+function ListNotes(){
+	document.write("Note ID": notes[0],
+	                notes[1], "By Author": notes[2])
+}
+function get(note_id){
+	for i in length(notes){
+		if (note_id == notes[0, i]){
+		    document.write(notes[1, i])
+	    }
+	}
 	
 }
-var note_content = function(){
-	notesList = list.push({note_content})
-	return
+function search(search_text){
+	
 }
-var ListNotes = 
+function delet(note_id){
+	
+}
+function edit(note_id, new_content){
+	
+}
