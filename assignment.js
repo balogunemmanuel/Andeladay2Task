92627d40c6cad0c4d13dc5b4f6ddc2c223afcd3d
Balogun Emmanuel <dewddo@gmail.com>
2016-05-17 17:03:48 +0100

    Delete function added

--- assignment.js
+++ assignment.js
@@ -18,19 +18,28 @@ function ListNotes(){
 	console.log("Note ID" + notes_id, notes_content, + "By Author" + notes_author);
 }
 function get(note_id){
-	len = notes.length;
+	len = notes_id.length;
 	for (var i in len){
 		if (note_id == notes_id[i]){
 		    console.log(notes_content[i]);
 	    }
 	}
-	
 }
 function search(search_text){
-	
+	len = note_content.length;
+	for (var i in len){
+		if (note_content.indexOf(search_text)!=-1) {
+            console.log(note_content[i]);
+		}
+    }
 }
 function delet(note_id){
-	
+	len = note_content.length;
+	for (var i in len){
+		if (note_id == notes_id[i]){
+            note_content[i].delete;
+		}
+    }
 }
 function edit(note_id, new_content){
 	
