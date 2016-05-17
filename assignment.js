3074222d8c1edabde88520c321f0f16a61598e61
Balogun Emmanuel <dewddo@gmail.com>
2016-05-17 15:50:52 +0100

    Create assignment.js

---
+++ assignment.js
@@ -0,0 +1,11 @@
+function NoteApplication(author) {
+	this.Author = author;
+	
+	var notes = []
+	
+}
+var note_content = function(){
+	notesList = list.push({note_content})
+	return
+}
+var ListNotes = 
